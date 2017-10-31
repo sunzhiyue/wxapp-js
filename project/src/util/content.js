cbb3c0496c1b18ccee0211badf146e29062cb25c
sunzhiyue <sunzhiue@outlook.com>
2017-10-31 20:48:54 +0800

    增加图片预览

--- project/src/util/content.js
+++ project/src/util/content.js
@@ -2,21 +2,27 @@ module.exports = [
 
   {
     recName: '海上日出',
-    recPhoto: 'https://img.25pp.com/uploadfile/bizhi/iphone6p/20160803/1470208048682044_390x690.jpg',
+    recPhoto: [
+      'https://img.25pp.com/uploadfile/bizhi/iphone6p/20160803/1470208048682044_390x690.jpg',
+      'https://img.25pp.com/uploadfile/bizhi/iphone6p/20160817/1471396486997569_390x690.jpg'
+    ],
     recNumber: 20,
     describe: '   屹立在岸边的沙滩上，向远处望去，只看见白茫茫的一片。海水和天空合为一体，都分不清是水还是天。正所谓：雾锁山头山锁雾，天连水尾水连天。远处的海水，在娇艳的阳光照耀下，像片片鱼鳞铺在水面，又像顽皮的小孩不断向岸边跳跃。'
   },
   {
     recName: '夜空的星',
-    recPhoto: 'https://img.25pp.com/uploadfile/bizhi/iphone6p/20151217/1450314843477172_390x690.jpg',
+    recPhoto: [
+      'https://img.25pp.com/uploadfile/bizhi/iphone6p/20151217/1450314843477172_390x690.jpg',
+      'https://img.25pp.com/uploadfile/bizhi/iphone6p/20160811/1470880073243114_390x690.jpg'
+    ],
     recNumber: 40
   },
-  { recName: '大山', recPhoto: 'https://img.25pp.com/uploadfile/bizhi/iphone6p/20160803/1470208050838529_390x690.jpg', recNumber: '30' },
-  { recName: '树', recPhoto: 'https://img.25pp.com/uploadfile/bizhi/iphone6p/20160817/1471396487530350_390x690.jpg', recNumber: '30' },
-  { recName: '飓风', recPhoto: 'https://img.25pp.com/uploadfile/bizhi/iphone6p/20160729/1469754685123964_390x690.jpg', recNumber: '30' },
+  { recName: '大山', recPhoto: ['https://img.25pp.com/uploadfile/bizhi/iphone6p/20160803/1470208050838529_390x690.jpg'], recNumber: '30' },
+  { recName: '树', recPhoto: ['https://img.25pp.com/uploadfile/bizhi/iphone6p/20160817/1471396487530350_390x690.jpg'], recNumber: '30' },
+  { recName: '飓风', recPhoto: ['https://img.25pp.com/uploadfile/bizhi/iphone6p/20160729/1469754685123964_390x690.jpg'], recNumber: '30' },
   {
     recName: '光',
-    recPhoto: 'https://img.25pp.com/uploadfile/bizhi/iphone6p/20160713/1468372521929333_390x690.jpg',
+    recPhoto: ['https://img.25pp.com/uploadfile/bizhi/iphone6p/20160713/1468372521929333_390x690.jpg'],
     recNumber: 22,
     describe: ''
   }
